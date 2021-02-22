6a589f01839110c3325297a190cce455c19485ec
Grant Patterson <darkrider231@gmail.com>
2021-02-22 14:21:45 -0500

    JSBasicsExercise Completed

---
+++ jsBasicsExercise.js
@@ -0,0 +1,50 @@
+// #1
+console.log("34");
+
+// #2
+// Numbers, Strings, Booleans, null and undefined
+
+// #3
+// +, -, *, /, **, and %
+
+// #4
+console.log(5%2);
+console.log(5%4);
+
+// #5
+console.log('I\'M MARRY POPPINS, Y\'ALL!');
+
+// #6
+console.log("Wanda" + "Vision");
+
+// #7
+console.log("Grant".length + "Patterson".length);
+
+// #8
+console.log("Avengers: Endgame"[2]);
+
+// BONUS
+// #1
+console.log("\\");
+
+// #2
+console.log(5**4-9);
+
+// #3
+console.log("I".length % "Am".length % "Iron".length % "Man".length - 1);
+
+// #4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("HAWKEYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// #5
+var m = 10;
+var c = 5;
+var u = m + c;
+console.log(u);
